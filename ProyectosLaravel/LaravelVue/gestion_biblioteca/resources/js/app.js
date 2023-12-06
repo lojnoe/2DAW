f622424f818d6176b69c61f39d33698086ce99dc
lojnoe <lojnoe999@gmail.com>
2023-12-06 22:56:54 +0100

    Laravel / Vue

--- ProyectosLaravel/LaravelVue/gestion_biblioteca/resources/js/app.js
+++ ProyectosLaravel/LaravelVue/gestion_biblioteca/resources/js/app.js
@@ -1,8 +1,12 @@
 import './bootstrap';
 import { createApp } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'
-import BookList from '@/components/BookList.vue'
+import HelloWorld from '@/components/HelloWorld.vue';
+import BookList from '@/components/BookList.vue';
 // crear aplicacion
-import router from "./router"
+import router from "./router";
+
+
+import '../css/app.css';
+
 
 createApp(HelloWorld).use(router).mount("#app");
