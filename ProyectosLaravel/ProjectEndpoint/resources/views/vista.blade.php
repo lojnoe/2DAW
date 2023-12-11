ca178d9804eb328ff6ed8015998ebb9d7169b0b6
lojnoe <lojnoe999@gmail.com>
2023-12-11 20:12:08 +0100

    Con estilo

--- ProyectosLaravel/ProjectEndpoint/resources/views/vista.blade.php
+++ ProyectosLaravel/ProjectEndpoint/resources/views/vista.blade.php
@@ -5,14 +5,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ejemplo con AJAX</title>
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body>
+<body class="bg-gray-800 text-white">
 
-    <h1>Prueba ajax</h1>
+    <h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Prueba ajax</h1>
 
     <!-- Botón para enviar la petición AJAX -->
-    <button id="cargarDatos">Cargar Datos</button>
-    <div id="resultado"> <!-- Corrección: eliminé el signo de numeral (#) -->
+    <button id="cargarDatos"  class="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">Cargar Datos</button>
+    <div id="resultado" class="mb-4 text-lg font-normal text-gray-500 dark:text-gray-400" >
 
     </div>
     <script>
